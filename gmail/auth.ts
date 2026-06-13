07f013ad26645a6c99c9bda89d95d8ae77596187
sbokhari <bokharisaab12345@gmail.com>
2026-06-13 02:04:54 -0400

    user can now verify google oauth to create token

--- gmail/auth.ts
+++ gmail/auth.ts
@@ -1,19 +1,50 @@
-import { authenticate } from "@google-cloud/local-auth"
-import { google } from "googleapis"
+import { google } from "googleapis";
+import readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+import { writeFile } from "fs/promises";
 
-const clientId = process.env.GOOGLE_CLIENT_ID;
-const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
-const redirect = process.env.GOOGLE_REDIRECT_URI;
+const GMAIL_SCOPE = "https://www.googleapis.com/auth/gmail.readonly";
+
 function createOAuthClient() {
-  if (!clientId || !clientSecret || !redirect) {
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+  const redirectUri = process.env.GOOGLE_REDIRECT_URI;
+
+  if (!clientId || !clientSecret || !redirectUri) {
     throw new Error(
-      "Missing required Gmail API environment variables. " +
-        "Please check GMAIL_CLIENT_ID, GMAIL_CLIENT_SECRET, and GMAIL_REDIRECT_URI.",
+      "Missing required env vars: GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URI",
     );
   }
 
-  return new google.auth.OAuth2(clientId, clientSecret, redirect);
+  return new google.auth.OAuth2(clientId, clientSecret, redirectUri);
 }
-const oauth2Client = createOAuthClient();
 
-export default function authenticateGmail() {}
+export default async function authenticateGmail() {
+  const oauth2Client = createOAuthClient();
+
+  const authUrl = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    prompt: "consent",
+    scope: [GMAIL_SCOPE],
+  });
+
+  console.log("\nOpen this URL in your browser to sign in:\n");
+  console.log(authUrl);
+
+  const rl = readline.createInterface({ input, output });
+  try {
+    const code = (
+      await rl.question(
+        "\nPlease paste the code found in the URL after browser login. This will be exchanged for a real token.\n",
+      )
+    ).trim();
+    const { tokens } = await oauth2Client.getToken(code);
+    const jsonString = JSON.stringify(tokens, null, 2);
+    await writeFile("token.json", jsonString, "utf8");
+    console.log("Successfully created token.")
+  } catch (error) {
+    console.error("Error with authenticating your code", error);
+  } finally {
+    rl.close();
+  }
+}
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
+import "dotenv/config";
 import { Command } from "commander";
 import readApplicationData from "./storage/applications.js";
-import authenticateGmail from "./gmail/auth.js"
+import authenticateGmail from "./gmail/auth.js";
 
 const program = new Command();
 
@@ -8,11 +9,14 @@ program
   .command("list")
   .description("outputs a table with fake application data")
   .action(async () => {
-    console.table(await readApplicationData())
+    console.table(await readApplicationData());
   });
 
-program.command("auth").description("Connect your Gmail account").action(async() => {
-  authenticateGmail()
-})
+program
+  .command("auth")
+  .description("Connect your Gmail account")
+  .action(async () => {
+    await authenticateGmail();
+  });
 
 program.parse(process.argv);
